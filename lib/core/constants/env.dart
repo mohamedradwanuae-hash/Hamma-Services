b3891bd4c5ff92ae9054546c89cf65d488595493
mohamedradwanuae-hash <mohamedradwanuae@gmail.com>
2026-07-22 14:45:37 +0000

    Initial commit: Hamma Services Flutter App

--- lib/core/constants/env.dart
+++ lib/core/constants/env.dart
@@ -1,10 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
+import { initializeApp } from "firebase/app";
 class Env {
   static const String supabaseUrl = 'YOUR_SUPABASE_URL';
   static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
-  static const FirebaseOptions firebaseOptions = FirebaseOptions(
-    apiKey: 'YOUR_FIREBASE_API_KEY', appId: 'YOUR_FIREBASE_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID', projectId: 'YOUR_FIREBASE_PROJECT_ID',
-  );
+  // Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzDWfzG1hNoEqXKhZQOpVuHPH5PnU64iQ",
+  authDomain: "hammaservicesuk-43e3b.firebaseapp.com",
+  projectId: "hammaservicesuk-43e3b",
+  storageBucket: "hammaservicesuk-43e3b.firebasestorage.app",
+  messagingSenderId: "650845530729",
+  appId: "1:650845530729:web:df4d5a3655b20397c9d0f8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   static const String stripePublishableKey = 'YOUR_STRIPE_PUBLISHABLE_KEY';
 }
